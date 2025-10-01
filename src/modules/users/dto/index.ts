73557b56de9a6d2de7b3c4f9520953543021da47
ThatCoolGuyyy <goldendarmie@gmail.com>
2025-10-01 14:09:00 +0100

    feat: add barrel exports for user DTOs and entities

---
+++ src/modules/users/dto/index.ts
@@ -0,0 +1,8 @@
+export * from './create-user.dto';
+export * from './update-user.dto';
+export * from './login-user.dto';
+export * from './oauth-user.dto';
+
+export * from './change-password.dto';
+export * from './forgot-password.dto';
+export * from './reset-password.dto';
---
+++ src/modules/users/entities/index.ts
@@ -0,0 +1,4 @@
+// Entities barrel export
+export * from './user.entity';
+export * from './user-meta.entity';
+export * from './user-auth-provider.entity';
