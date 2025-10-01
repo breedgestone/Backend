4aa8c8849fa9137261f1f6f01626ae33ca071ffd
ThatCoolGuyyy <goldendarmie@gmail.com>
2025-10-01 14:04:53 +0100

    feat: restructure app module imports and add global configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { DatabaseModule } from './database.module';
+import { NotificationModule } from './modules/notification/notification.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    DatabaseModule,
+    AuthModule, 
+    UsersModule, 
+    NotificationModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
